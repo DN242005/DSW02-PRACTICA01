2cb49ab8819f47788b476c8fbb433c3b60e0b09d
Jonathan Daniel Paz Sanchez <zs23020598@estudiantes.uv.mx>
2026-05-25 07:39:37 -0600

    Add minimal frontend and backend tests

---
+++ src/test/java/com/prcatica01/empleado/EmpleadoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.prcatica01.empleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
