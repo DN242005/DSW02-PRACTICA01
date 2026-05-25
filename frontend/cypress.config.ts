a25706b73122a441fa8ddd8e7028c2aa160ca152
Jonathan Daniel Paz Sanchez <zs23020598@estudiantes.uv.mx>
2026-05-25 07:52:29 -0600

    Fix Cypress baseUrl for CI

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://127.0.0.1:4200',
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: false,
     retries: {
